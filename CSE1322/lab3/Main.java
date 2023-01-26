c9881604f96c5684d921cc711508cf8c414db179
Rob <95727104+mnfu@users.noreply.github.com>
2023-01-26 12:09:17 -0500

    add lab3 work

---
+++ CSE1322/lab3/Main.java
@@ -0,0 +1,137 @@
+package lab3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean active = true;
+        Quiz quiz = new Quiz();
+        while(active) {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Add a question to the quiz");
+            System.out.println("2. Remove a question from the quiz");
+            System.out.println("3. Modify a question in the quiz");
+            System.out.println("4. Take the quiz");
+            System.out.println("5. Quit");
+            Scanner input = new Scanner(System.in);
+            String userInput = input.nextLine();
+            if (userInput.equals("1")) {
+                quiz.addQuestion();
+            }
+            else if (userInput.equals("2")) {
+                quiz.removeQuestion();
+            }
+            else if (userInput.equals("3")) {
+                quiz.modifyQuestion();
+            }
+            else if (userInput.equals("4")) {
+                quiz.giveQuiz();
+            }
+            else if (userInput.equals("5")) {
+                active = false;
+            }
+        }
+    }
+}
+
+class Question {
+    private String question = "";
+    private String answer = "";
+    int difficulty = 0;
+
+    public Question(String q, String a, int d) {
+        question = q;
+        answer = a;
+        difficulty = d;
+    }
+
+    public void setQuestion(String q) {
+        question=q;
+    }
+
+    public void setAnswer(String a) {
+        answer=a;
+    }
+
+    public void setDifficulty(int d) {
+        difficulty=d;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+}
+
+class Quiz {
+    ArrayList<Question> Quiz = new ArrayList<Question>();
+    Scanner input = new Scanner(System.in);
+    public void addQuestion() {
+        System.out.println("What is the question text?");
+        String qQuestion = input.nextLine();
+        System.out.println("What is the answer?");
+        String qAnswer = input.nextLine();
+        System.out.println("How Difficult (1-3)?");
+        int qDifficulty = Integer.parseInt(input.nextLine());
+        if((qDifficulty != 1) && (qDifficulty != 2) && (qDifficulty != 3)) {
+            System.out.println("ERROR, NOT 1-3. QUESTION CREATION FAILED");
+        }
+        Question newQ = new Question(qQuestion, qAnswer, qDifficulty);
+        Quiz.add(newQ);
+    }
+
+    public void removeQuestion() {
+        System.out.println("Choose the question to remove?");
+        int num = 0;
+        for(Question q : Quiz) {
+            System.out.println(num + ". " + q.getQuestion());
+            num++;
+        }
+        Quiz.remove(Integer.parseInt(input.nextLine()));
+    }
+
+    public void modifyQuestion() {
+        System.out.println("Choose the question to modify?");
+        int num = 0;
+        for(Question q : Quiz) {
+            System.out.println(num + ". " + q.getQuestion());
+            num++;
+        }
+        int qNum = (Integer.parseInt(input.nextLine()));
+        System.out.println("What is the question text?");
+        Quiz.get(qNum).setQuestion(input.nextLine());
+        System.out.println("What is the answer?");
+        Quiz.get(qNum).setAnswer(input.nextLine());
+        System.out.println("How Difficult (1-3)?");
+        int qDifficulty = Integer.parseInt(input.nextLine());
+        if((qDifficulty != 1) && (qDifficulty != 2) && (qDifficulty != 3)) {
+            System.out.println("ERROR, NOT 1-3. QUESTION DIFFICULTY WAS NOT CHANGED");
+        }
+        else {
+            Quiz.get(qNum).setDifficulty(qDifficulty);
+        }
+    }
+
+    public void giveQuiz() {
+        int score = 0;
+        for(Question q : Quiz) {
+            System.out.println(q.getQuestion());
+            if (input.nextLine().equals(q.getAnswer())) {
+                System.out.println("Correct");
+                score++;
+            }
+            else {
+                System.out.println("Incorrect");
+            }
+        }
+        System.out.println("You got " + score + " out of " + Quiz.size());
+    }
+}
