2535fa209722a3af4df7d4fdeab1b2f8e69f72cf
Rob <95727104+mnfu@users.noreply.github.com>
2023-02-16 19:25:00 -0500

    Update FibIteration.java

--- CSE1322/lab6/FibIteration.java
+++ CSE1322/lab6/FibIteration.java
@@ -8,14 +8,14 @@ public class FibIteration implements FindFib{
             return 1;
         }
         else {
-            int previous = 0;
-            int current = 1;
+            int previous = 0; //tracks previous fib num
+            int current = 1; //tracks current fib num
             for(int i = 0; i < num; i++) {
-                int tempprev = current;
-                current = previous + current;
-                previous = tempprev;
+                int tempprev = current; //creates a save of the current num
+                current = previous + current; //current is changed
+                previous = tempprev; //previous num is set to the old current num
             }
-            if(previous == Integer.MAX_VALUE || previous < 0){
+            if(previous == Integer.MAX_VALUE || previous < 0){ //Make sure that the integer doesn't do wacky stuff past 32b lim
                 return Integer.MAX_VALUE;
             }
             return previous;
