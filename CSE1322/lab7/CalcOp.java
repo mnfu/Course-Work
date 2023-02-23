4732c9b50830ff74d0b3ef0422ec4b2bf1264845
Rob <95727104+mnfu@users.noreply.github.com>
2023-02-23 11:27:39 -0500

    Create CalcOp.java

---
+++ CSE1322/lab7/CalcOp.java
@@ -0,0 +1,8 @@
+package lab7;
+
+public interface CalcOp {
+    float add(float n1, float n2);
+    float subtract(float n1, float n2);
+    float multiply(float n1, float n2);
+    float divide(float n1, float n2);
+}
