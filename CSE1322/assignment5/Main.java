3e1949a526208128a1a0504627da069334c75359
Rob <95727104+mnfu@users.noreply.github.com>
2023-03-14 15:48:11 -0400

    create assignment5 & lab8 for future use

---
+++ CSE1322/assignment5/Main.java
@@ -0,0 +1,7 @@
+package assignment5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("assignment5 is unfinished");
+    }
+}
---
+++ CSE1322/lab8/Main.java
@@ -0,0 +1,7 @@
+package lab8;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("lab8 is unfinished");
+    }
+}
