3fbe5026769324a93d8666c107b3601353792a0b
Rob <95727104+mnfu@users.noreply.github.com>
2023-04-11 19:01:05 -0400

    assign first disk in collection to first node in linked list instead of leaving it null

--- CSE1322/lab12/BlueRayCollection.java
+++ CSE1322/lab12/BlueRayCollection.java
@@ -7,6 +7,9 @@ public class BlueRayCollection {
 
     public void add(String title, String director, int yearOfRelease, double cost) {
         Node current = head;
+        if(current.data == null) {
+            current.data = new BlueRayDisk(title, director, yearOfRelease, cost);
+        }
         while(current.next != null){
             current = current.next;
         }
@@ -16,7 +19,7 @@ public class BlueRayCollection {
     public void show_all() {
         Node current = head;
         while(current.next != null){
-            System.out.println(current.next.data.toString());
+            System.out.println(current.data.toString());
             current = current.next;
         }
     }
