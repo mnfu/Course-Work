48fb568904c3325eb22d083e0b63b3c65f373adc
Rob <95727104+mnfu@users.noreply.github.com>
2023-09-02 18:51:12 -0400

    initial lab3 work, doesn't currently pass autograder

---
+++ IT1114/Lab3.py
@@ -0,0 +1,34 @@
+# Course: IT1114/Section W05
+# Student Name: Rob Ives
+# Assignment Number: Lab3
+# Due Date: 09/10/2023
+# Purpose: To calculate manager bonuses & total sales for the month
+# Resources: Lecture Videos & docs.python.org
+
+# User Input
+salesGoal = int(input("Sales goal: "))
+
+# Default Values
+sales = 0
+salesPerson = 1
+
+# User Input Loops
+while True:
+    for x in range(4):
+        sales += int(input("Salesperson " + str(salesPerson) + " week " + str(x+1) + ": "))
+    another = input("Another salesperson?: ")
+    if another == "n":
+        break
+    salesPerson += 1
+
+# Calculations
+managerBonus = sales * 0.02
+if sales > salesGoal:
+    managerBonus = sales * 0.05
+
+# Output print statements
+print("\nDepartment Monthly Sales and Commission\n")
+print("Number of Employees: " + str(salesPerson))
+print("Department Sales Goal: $" + str(salesGoal))
+print("Total Sales: $" + str('{:,.2f}'.format(sales)))
+print("Mgr. Bonus: $" + str('{:,.2f}'.format(managerBonus)))
